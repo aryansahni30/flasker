3bd4fc400b767697a5a10cfbb59511253dc7efc5
Aryan Sahni <aryansahni@Aryans-MacBook-Pro.local>
2022-09-08 18:18:42 -0700

    First server

---
+++ app.py
@@ -0,0 +1,19 @@
+# Create a simple flask app
+from flask import Flask, render_template
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
+@app.route('/')
+def hello_world():
+    return render_template('index.htm')
+
+@app.route('/about')
+def hello_world_about():
+    return render_template('about.html')
+
+@app.route('/edu')
+def hello_world_edu():
+    return render_template('edu.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
